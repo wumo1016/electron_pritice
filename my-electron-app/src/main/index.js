6b5123b164c82210c9d6b4af975e3a780a0d8ed5
wumo1016 <760478279@qq.com>
2023-05-05 10:27:57 +0800

    init: 应用协议唤起

--- my-electron-app/src/main/index.js
+++ my-electron-app/src/main/index.js
@@ -1,14 +1,53 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-let mainWindow
+let mainWindow,
+  urlParams = {}
+
+const protocol = 'zhufeng'
+const scheme = `${protocol}://`
+app.setAsDefaultProtocolClient(protocol)
+
 app.whenReady().then(() => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    titleBarStyle: 'hiddenInset'
-  })
-  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
-  // 打开调试控制台
-  mainWindow.webContents.openDevTools()
+  createWindow()
 })
+/**
+ * @Author: wyb
+ * @Descripttion: 创建 window
+ */
+function createWindow() {
+  const width = parseInt(urlParams.width) || 800
+  const height = parseInt(urlParams.height) || 600
+  if (mainWindow) {
+    mainWindow.setSize(width, height)
+  } else {
+    mainWindow = new BrowserWindow({ width, height })
+    mainWindow.loadFile(path.resolve(__dirname, '../renderer/index.html'))
+  }
+}
+/**
+ * @Author: wyb
+ * @Descripttion:
+ * @param {*} argv
+ */
+function handleSchemeWakeup(argv) {
+  const url = [].concat(argv).find(v => v.startsWith(scheme))
+  if (!url) return
+  const searchParams = new URLSearchParams(url.slice(scheme.length))
+  urlParams = Object.fromEntries(searchParams.entries())
+  if (app.isReady()) createWindow()
+}
+/**
+ * @Author: wyb
+ * @Descripttion: https://www.electronjs.org/zh/docs/latest/api/app#apprequestsingleinstancelockadditionaldata
+ */
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, argv) => {
+    mainWindow.restore()
+    mainWindow.show()
+    handleSchemeWakeup(argv)
+  })
+}
