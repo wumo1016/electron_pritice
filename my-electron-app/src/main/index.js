dd4f2e7094a64fc26d020245cf93b549e045ff68
wumo1016 <760478279@qq.com>
2023-05-10 10:15:23 +0800

    feat: 系统托盘

--- my-electron-app/src/main/index.js
+++ my-electron-app/src/main/index.js
@@ -1,8 +1,54 @@
-const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+const { app, BrowserWindow, Tray, Menu } = require('electron')
 const path = require('path')
+
+let tray
+
+const menus = [
+  {
+    label: '关于',
+    role: 'about'
+  },
+  { type: 'separator' },
+  {
+    label: '菜单2',
+    click: () => {
+      console.log('点击了菜单2')
+    }
+  },
+  {
+    label: '子菜单',
+    submenu: [
+      {
+        label: '显示窗口',
+        click: () => {
+          console.log('点击了二级菜单')
+        }
+      },
+      {
+        label: '打开控制台',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  { type: 'separator' },
+  {
+    label: '退出',
+    role: 'quit'
+  }
+]
+const contextMenu = Menu.buildFromTemplate(menus)
 
 app.whenReady().then(() => {
   createWindow()
+  // 可创建多个系统托盘
+  tray = new Tray(path.join(__dirname, '../images/phoneTemplate.png'))
+  tray.setContextMenu(contextMenu) // 设置托盘菜单
+  tray.setToolTip('我是一个小提示呀') // 设置托盘图标提示
+  //  可以设置 2秒之后，自动弹出托盘菜单 但是会显示在鼠标位置
+  setTimeout(() => {
+    const popUpMenu = Menu.buildFromTemplate(menus.slice(1))
+    tray.popUpContextMenu(popUpMenu)
+  }, 2000)
 })
 /**
  * @Author: wyb
@@ -22,28 +68,3 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
   mainWindow.webContents.openDevTools()
 }
-
-ipcMain.on('open-menu', (event, ...args) => {
-  const menu = Menu.buildFromTemplate([
-    {
-      label: '文字加粗',
-      click: () => {
-        event.reply('set-menu', 'bold')
-      }
-    },
-    {
-      label: '改变颜色',
-      click: () => {
-        event.reply('set-menu', 'color')
-      }
-    }
-  ])
-  menu.popup({
-    // window: BrowserWindow.getFocusedWindow(),
-    // x: 10, // 相对于窗口的x轴偏移，默认是鼠标位置的横坐标
-    // y: 20, // 相对于窗口的y轴偏移，默认是鼠标位置的纵坐标
-    callback: () => {
-      console.log('menu closed')
-    }
-  })
-})
--- my-electron-app/src/preload/index.js
+++ my-electron-app/src/preload/index.js
@@ -1,16 +1,3 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge } = require('electron')
 
-contextBridge.exposeInMainWorld('preloadApi', {
-  openMenu() {
-    ipcRenderer.send('open-menu')
-  }
-})
-
-ipcRenderer.on('set-menu', (event, value) => {
-  if (value === 'bold') {
-    const button = document.querySelector('#button')
-    button.style.fontWeight = 'bold'
-  } else if (value === 'color') {
-    button.style.color = 'blue'
-  }
-})
+contextBridge.exposeInMainWorld('preloadApi', {})
