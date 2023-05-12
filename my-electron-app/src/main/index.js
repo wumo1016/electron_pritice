6344e7c982323184422358b192f99cbaba5e7d1e
wumo1016 <760478279@qq.com>
2023-05-12 09:42:41 +0800

    feat: webview

--- my-electron-app/src/main/index.js
+++ my-electron-app/src/main/index.js
@@ -1,30 +1,12 @@
-const { app, BrowserWindow, Tray, screen } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-let tray,
-  mainWindow,
-  width = 300,
-  height = 420
+let mainWindow,
+  width = 1000,
+  height = 800
 
 app.whenReady().then(() => {
-  const primaryDisplay = screen.getPrimaryDisplay()
-  const { height: screenHeight } = primaryDisplay.workAreaSize
   createWindow()
-  // 可创建多个系统托盘
-  tray = new Tray(path.join(__dirname, '../images/phoneTemplate.png'))
-  const trayBounds = tray.getBounds()
-  tray.on('right-click', () => {
-    console.log(mainWindow.isVisible())
-    if (mainWindow.isVisible()) {
-      mainWindow.hide()
-    } else {
-      mainWindow.setPosition(
-        trayBounds.x + trayBounds.width / 2 - width / 2,
-        screenHeight - height
-      )
-      mainWindow.show()
-    }
-  })
 })
 /**
  * @Author: wyb
@@ -34,23 +16,15 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width,
     height,
-    frame: false,
-    resizable: false,
-    show: false,
-    movable: false,
-    minimizable: false,
-    maximizable: false,
     webPreferences: {
+      webviewTag: true, // 需要添加此行
       nodeIntegration: false,
       contextIsolation: true,
       sandbox: false,
       preload: path.join(__dirname, '../preload/index.js')
     }
   })
-  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
-  // mainWindow.webContents.openDevTools()
-
-  mainWindow.on('blur', () => {
-    mainWindow.hide()
-  })
+  // mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'))
+  mainWindow.loadURL(' http://127.0.0.1:5173/')
+  mainWindow.webContents.openDevTools()
 }
---
+++ my-electron-app/src/renderer/src/App.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div class="toolbar">
+      <div :class="['back', { active: canGoBack }]" @click="goBack">&lt;</div>
+      <div :class="['forward', { active: canGoForward }]" @click="goForward">
+        &gt;
+      </div>
+      <input
+        v-model="url"
+        placeholder="Please enter the url"
+        @keydown.enter="go"
+      />
+      <div class="go" @click="go">Go</div>
+    </div>
+    <webview
+      ref="webview"
+      class="webview"
+      src="about:blank"
+      allowpopups
+    ></webview>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const url = ref('')
+const webview = ref(null)
+
+const webviewDomReady = ref(false)
+const canGoBack = ref(false)
+const canGoForward = ref(false)
+
+onMounted(() => {
+  const el = webview.value
+  if (!el) return
+  el.addEventListener('dom-ready', () => {
+    console.log('dom-ready')
+    webviewDomReady.value = true
+    updateNavigationState()
+    console.log('current url', el.getURL())
+  })
+  el.addEventListener('did-start-loading', event => {
+    console.log('did-start-loading', event)
+    updateNavigationState()
+  })
+  el.addEventListener('did-stop-loading', event => {
+    console.log('did-stop-loading', event)
+    updateNavigationState()
+  })
+  el.addEventListener('did-start-navigation', event => {
+    console.log('did-start-navigation', event)
+    updateNavigationState()
+    if (event.url.startsWith('http')) {
+      url.value = event.url
+    }
+  })
+  el.addEventListener('will-navigate', event => {
+    console.log('will-navigate', event)
+    url.value = event.url
+    go()
+  })
+  el.addEventListener('new-window', event => {
+    console.log('will-navigate', event)
+    url.value = event.url
+    go()
+  })
+})
+
+const updateNavigationState = () => {
+  if (!webview.value) return
+  if (!webviewDomReady.value) return
+  canGoBack.value = webview.value.canGoBack()
+  canGoForward.value = webview.value.canGoForward()
+}
+
+const isURL = input => {
+  const pattern = /^(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/
+  if (pattern.test(input)) return true
+  return pattern.test(`http://${input}`)
+}
+
+const goBack = () => {
+  const el = webview.value
+  if (el.canGoBack()) {
+    el.goBack()
+  }
+}
+
+const goForward = () => {
+  const el = webview.value
+  if (el.canGoForward()) {
+    el.goForward()
+  }
+}
+
+function go() {
+  const el = webview.value
+  const site = url.value || 'about:blank'
+  if (isURL(site)) {
+    el.loadURL(site.indexOf('://') === -1 ? `http://${site}` : site)
+  }
+}
+</script>
+
+<style scoped>
+.toolbar {
+  display: flex;
+  align-items: center;
+  height: 50px;
+  box-sizing: border-box;
+}
+
+.toolbar div {
+  border-radius: 5px;
+  background-color: #eeeeee;
+  margin: 3px;
+  padding: 2px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #d8d8d8;
+  user-select: none;
+}
+
+input {
+  flex: 1;
+  margin: 0 5px;
+  height: 26px;
+  border: 1px solid #d8d8d8;
+  outline: none;
+  padding: 0 10px;
+}
+
+.back,
+.forward {
+  width: 20px;
+  color: #babcbe;
+}
+
+.back.active,
+.forward.active {
+  color: black;
+}
+
+.go {
+  width: 50px;
+}
+
+.webview {
+  width: 100%;
+  height: calc(100vh - 50px);
+}
+</style>
